e0eacb39e36033e4234a34f83b5b37cd65e8b9c4
kasia251011 <katarzyna.jankowska000728@gmail.com>
2023-04-01 11:36:06 +0200

    Fix code errors

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
